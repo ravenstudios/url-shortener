3f0f104927a2d009246370dcad5d26435a400c72
ravenstudios <ravenstudiosrob@yahoo.com>
2017-11-06 19:58:38 -0600

    added views and jade, Project finished

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require("express");
 var addUrl = require("./addUrl");
 var getUrl = require("./getUrl");
+var path = require("path");
+
 
 
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 
 app.get("/:shortenedUrl", function(req, res){
-
-
   getUrl(req.params.shortenedUrl, function(result){
-
     var pattern = new RegExp(/http[s]?:\/\/www\.\w*.*/g);
     if(pattern.test(result)){
       res.statusCode = 302;
@@ -17,16 +24,15 @@ app.get("/:shortenedUrl", function(req, res){
       res.end();
     }
     else{
-      res.send(result);
+      res.render('index', { message: result });
     }
   });
-
 });
 
 app.get("/new/:url(*)", function(req, res){
 
   addUrl(req.params.url, function(result){
-    res.send(result);
+    res.render('index', { message: result });
   });
 });
 
@@ -34,3 +40,9 @@ app.get("/new/:url(*)", function(req, res){
 var listener = app.listen(3000, function () {
   console.log("server started");
 });
+
+
+// ******used for glitch************
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
